300764a0d52680a91878bd48cf3a1df82fd86c6c
GinormousSalmon <terleczcky.alex@gmail.com>
2020-10-20 03:42:02 +0300

    added draw condition (if three kings (or more) against one opponent's king, in 15 moves will not take the opponent's king)

--- src/main/kotlin/Desk.kt
+++ src/main/kotlin/Desk.kt
@@ -6,7 +6,10 @@ import tornadofx.hide
 
 class Desk(private val tiles: MutableList<MutableList<Rectangle>>) {
     private val desk = mutableListOf<MutableList<Checker?>>()
-    private var fifteenTurnsCount = 0
+    private var fifteenOnlyKingsTurnsCount = 0
+    private var fifteenTurnsWithoutAttackCount = 0
+    private var blackKingsCount = 0
+    private var whiteKingsCount = 0
 
     init {
         desk.clear()
@@ -30,26 +33,46 @@ class Desk(private val tiles: MutableList<MutableList<Rectangle>>) {
         desk[xTo][yTo] = desk[xFrom][yFrom]
         desk[xFrom][yFrom] = null
         val enemy = desk[xTo][yTo]?.possibleMoves?.filter { it.first == xTo && it.second == yTo }?.get(0)?.third
+        if (enemy == null || desk[enemy.first][enemy.second] !is King) {
+            println("white $whiteKingsCount black $blackKingsCount count $fifteenTurnsWithoutAttackCount")
+            if (whiteKingsCount == 1 && blackKingsCount >= 3 || blackKingsCount == 1 && whiteKingsCount >= 3)
+                fifteenTurnsWithoutAttackCount += 1
+            else
+                fifteenTurnsWithoutAttackCount = 0
+        }
         if (enemy != null) {
             remove(enemy)
             return true
         }
         if (desk[xTo][yTo] is King)
-            fifteenTurnsCount += 1
+            fifteenOnlyKingsTurnsCount += 1
         else
-            fifteenTurnsCount = 0
+            fifteenOnlyKingsTurnsCount = 0
         return false
     }
 
     fun makeKing(x: Int, y: Int) {
+        if (desk[x][y] is King)
+            return
         val turn = desk[x][y]?.color
         desk[x][y] = King(desk[x][y]!!)
         val col = if (turn == WHITE) "white" else "black"
         desk[x][y]?.image?.image = Image("file:src/main/resources/${col}_king.png")
+        if (turn == WHITE)
+            whiteKingsCount += 1
+        else
+            blackKingsCount += 1
     }
 
     private fun remove(xy: Pair<Int, Int>) {
         val (x, y) = xy
+        if (desk[x][y] is King) {
+            val color = desk[x][y]?.color
+            if (color == WHITE)
+                whiteKingsCount -= 1
+            else
+                blackKingsCount -= 1
+        }
         desk[x][y]?.image?.hide()
         desk[x][y] = null
     }
@@ -72,6 +95,6 @@ class Desk(private val tiles: MutableList<MutableList<Rectangle>>) {
     }
 
     fun draw(): Boolean {
-        return fifteenTurnsCount == 15
+        return fifteenTurnsWithoutAttackCount == 15 || fifteenOnlyKingsTurnsCount == 15
     }
 }
